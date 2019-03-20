c9a7590237e4c402ed9a3424f6a4e740a2511752
KMcArthur <kmcarthur7878@gmail.com>
2019-03-20 16:43:25 -0400

    first commit

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,29 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Kelsey McArthur on 3/19/19.
+//  Copyright © 2019 KMcArthur78. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
